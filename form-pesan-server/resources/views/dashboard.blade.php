ea72ae070a569be3a9bd16fb01b4f5a22a9b5f62
Raihan Hammam <hammamraihan31@gmail.com>
2025-01-20 10:28:38 +0700

    Modify Server Display

--- form-pesan-server/resources/views/dashboard.blade.php
+++ form-pesan-server/resources/views/dashboard.blade.php
@@ -8,21 +8,33 @@
 
                     @forelse ($messages as $message)
                         <div class="card">
-                            <div class="card-header">Message From {{ $message->name }}</div>
+                            <div class="card-header">
+                                <div class="row justify-content-between">
+                                    <span class="text-start">Message From {{ $message->name }} ( {{ $message->created_at->diffForHumans() }} )</span>
+                                    <span class="text-start text-primary">On {{ $message->created_at->format('F d, Y, \\a\\t h:i A')}} </span>    
+                                </div>
+                                
+                            </div>
                             <div class="card-body">
+                                <h5 class="card-title text-center fw-bold">" {{ $message->subject }} "</h5>
                                 <p>Client Email : {{ $message->email }}</p>
                                 <p>Client Message : {{ $message->message }} </p>
                                 <form action="{{ route('dashboard.update', $message->id) }}" method="POST">
                                     @csrf
                                     @method('PUT')
-                                    <select name="status" class="form-select" onchange="this.form.submit()">
-                                        <option value="Pending" {{ $message->status == 'Pending' ? 'selected' : '' }}>
-                                            Menunggu Dibalas</option>
-                                        <option value="Responded" {{ $message->status == 'Responded' ? 'selected' : '' }}>
-                                            Sudah Dibalas</option>
-                                        <option value="Rejected" {{ $message->status == 'Rejected' ? 'selected' : '' }}>
-                                            Diabaikan / Tidak Valid</option>
-                                    </select>
+                                    <div class="form-floating">
+                                        <select name="status" class="form-select" id="status"
+                                            onchange="this.form.submit()">
+                                            <option value="Pending" {{ $message->status == 'Pending' ? 'selected' : '' }}>
+                                                Menunggu Dibalas</option>
+                                            <option value="Responded"
+                                                {{ $message->status == 'Responded' ? 'selected' : '' }}>
+                                                Sudah Dibalas</option>
+                                            <option value="Rejected" {{ $message->status == 'Rejected' ? 'selected' : '' }}>
+                                                Diabaikan / Tidak Valid</option>
+                                        </select>
+                                        <label for="status">Respond Status</label>
+                                    </div>
                                 </form>
                             </div>
                         </div>
